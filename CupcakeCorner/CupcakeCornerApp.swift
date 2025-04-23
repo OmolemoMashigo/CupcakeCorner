fbd7685c0330d1385a9f54b335084fc9bc0fe0c3
Omolemo Mashigo <omomashigo@gmail.com>
2025-04-23 19:36:31 +0200

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Omolemo Mashigo on 2025/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
