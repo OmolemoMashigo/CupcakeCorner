2c95f1986c5cc06553bc19926a425a5b98c3a767
Omolemo Mashigo <omomashigo@gmail.com>
2025-04-23 21:15:53 +0200

    using AsyncImage to load an image from the internet

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,50 +7,19 @@
 
 import SwiftUI
 
-struct Response: Codable{
-   var results: [Result]
-}
-
-struct Result: Codable{
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
     
-    @State private var results = [Result]()
     
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading){
-                Text(item.trackName)
-                    .font(.headline)
-                
-                Text(item.collectionName)
-            }
-            
-        }
-        .task {
-            await loadData()
-        }
-    }
-    
-    func loadData() async{
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else{
-            print("INVALID URL")
-            return
-        }
         
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data){
-                results = decodedResponse.results
-            }
-        }catch{
-            print("INVALID DATA")
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")){ image in
+            image
+                .resizable()
+                .scaledToFit()
+        } placeholder:{
+            ProgressView()
         }
+        .frame(width: 200, height: 200)
     }
 }
 
