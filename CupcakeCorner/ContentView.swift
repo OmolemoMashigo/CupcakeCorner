888015de849604855c24afcfaac8b7347cb00ad9
Omolemo Mashigo <omomashigo@gmail.com>
2025-04-23 21:19:18 +0200

    using phase to gain control of all the loading phases of an AsyncImage

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -12,12 +12,18 @@ struct ContentView: View {
     
     var body: some View {
         
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")){ image in
-            image
-                .resizable()
-                .scaledToFit()
-        } placeholder:{
-            ProgressView()
+        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")){ phase in
+            if let image = phase.image{
+                image
+                    .resizable()
+                    .scaledToFit()
+            }
+            else if phase.error == nil{
+                Text("THERE WAS AN ERROR LOADING THE IMAGE")
+            }else{
+                ProgressView()
+            }
+            
         }
         .frame(width: 200, height: 200)
     }
