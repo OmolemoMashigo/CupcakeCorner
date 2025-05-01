9d5d5cfd010401f4074a9308222aefd94308e790
Omolemo Mashigo <omomashigo@gmail.com>
2025-05-01 11:58:08 +0200

    creating network calls to send and recieve data over the internet

--- CheckoutView.swift
+++ CheckoutView.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    
     var order: Order
+    
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView{
             VStack{
@@ -25,6 +30,9 @@ struct CheckoutView: View {
                     .font(.title)
                 
                 Button("Place Order"){
+                    Task{
+                        await placeOrder()
+                    }
                     
                 }.padding()
             }
@@ -32,6 +40,37 @@ struct CheckoutView: View {
         .navigationTitle("Checkout")
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+        .alert("Thank you for your order!", isPresented: $showingConfirmation){
+            
+        }message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async{
+        guard let encoded = try? JSONEncoder().encode(order) else{
+            print("failed to encode order")
+            return
+        }
+        print("check 1")
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        print("check 2")
+        do{
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            print("check 3")
+            print(encoded.startIndex)
+            print(data)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            print("check 4")
+            confirmationMessage = "your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way"
+            showingConfirmation = true
+        }
+        catch{
+            print("checkout failed: \(error.localizedDescription)")
+        }
     }
 }
 
